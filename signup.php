0ef8ba8d9b5dd5e92928d09924b18fa52efbff2e
Thameem-20 <ahmedthameem20@gmail.com>
2024-11-10 19:44:41 +0530

    hello world

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <?php
 include("./config.php");
 session_start();
-if( $_SESSION['user_type'] ="faculty"){
+if( $_SESSION['user_type'] !="admin"){
     header("Location:./accessDenied.html");
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
